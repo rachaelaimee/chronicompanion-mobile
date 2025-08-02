20f8bbb8f6042ab9705437935da3d2b2644c14cb
Rachael Aimee <ivytenebrae1@gmail.com>
2025-08-02 20:22:12 +0100

    "Fix-plugin-registration"

--- android/app/src/main/java/io/github/rachaelaimee/chronicompanion/MainActivity.java
+++ android/app/src/main/java/io/github/rachaelaimee/chronicompanion/MainActivity.java
@@ -1,5 +1,14 @@
 package io.github.rachaelaimee.chronicompanion;
 
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Register Firebase Authentication plugin
+        registerPlugin(FirebaseAuthenticationPlugin.class);
+    }
+}
